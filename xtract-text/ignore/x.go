ba2391f3069fe9f3d8b992112e92e8c81f71667f
Nathan Marley <nathan.marley@gmail.com>
2019-11-25 18:57:56 -0300

    add lots of stuff - debug

---
+++ xtract-text/ignore/x.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	// "os"
+
+	// "rsc.io/pdf"
+    "github.com/ledongthuc/pdf"
+)
+
+func main() {
+	fn := "2019_11_21_ASSINADO_do1.pdf"
+	fmt.Println("file:", fn)
+
+	// f, err := os.Open(fn)
+	// if err != nil {
+	//     panic(err)
+	// }
+	// defer f.Close()
+
+	// r, err := pdf.NewReader(f,
+	// if err != nil {
+	//     panic(err)
+	// }
+
+    f, r, err := pdf.Open(fn)
+	if err != nil {
+		panic(err)
+	}
+    defer f.Close()
+
+	var buf bytes.Buffer
+    b, err := r.GetPlainText()
+    if err != nil {
+        panic(err)
+    }
+    buf.ReadFrom(b)
+
+	fmt.Println(buf.String())
+
+	r, err := pdf.Open(fn)
+	if err != nil {
+		panic(err)
+	}
+
+	numPages := r.NumPage()
+	fmt.Printf("there are %d pages\n", numPages)
+
+	pageNo := 37
+	page := r.Page(pageNo)
+
+	// fmt.Printf("page %d = %v\n", pageNo, p)
+	// fmt.Printf("page %d has %d Texts\n", pageNo, len(p.Content().Text))
+	//	for i := 1; i <= numPages; i++ {
+	//		page := r.Page(i)
+	// cont := page.Content()
+
+	keys := page.V.Keys()
+	for _, k := range keys {
+		fmt.Printf("k = %v\n", k)
+	}
+
+	c := page.V.Key("Contents")
+	fmt.Printf("c = %v, type: %v\n", c, c.Kind())
+
+	//cont := page.V.Key("Contents")
+	//fmt.Printf("page %d has %d Texts\n", i, len(cont.Text))
+	//fmt.Printf("page %d has %d Rects\n", i, len(cont.Rect))
+	// fmt.Printf("debug: %v\n", page.V.Kind())
+	// fmt.Printf("pdf.Dict: %d\n", pdf.Dict)
+	//	}
+	// r.GetPlainText()
+
+}
---
+++ xtract-text/read.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"bytes"
+	// "os"
+
+	// "rsc.io/pdf"
+    "github.com/ledongthuc/pdf"
+)
+
+func main() {
+	fn := "2019_11_21_ASSINADO_do1.pdf"
+	fmt.Println("file:", fn)
+
+	// f, err := os.Open(fn)
+	// if err != nil {
+	//     panic(err)
+	// }
+	// defer f.Close()
+
+	// r, err := pdf.NewReader(f,
+	// if err != nil {
+	//     panic(err)
+	// }
+
+    f, r, err := pdf.Open(fn)
+	if err != nil {
+		panic(err)
+	}
+    defer f.Close()
+
+	var buf bytes.Buffer
+    b, err := r.GetPlainText()
+    if err != nil {
+        panic(err)
+    }
+    buf.ReadFrom(b)
+
+	fmt.Println(buf.String())
+}
