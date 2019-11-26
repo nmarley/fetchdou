61405a76bf38c57a9a84572f6c6c60d3de8d82de
Nathan Marley <nathan.marley@gmail.com>
2019-11-26 00:34:47 -0300

    Fix shit

--- pars/p.go
+++ pars/p.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	"fmt"
+	"html"
 	"io/ioutil"
 	"regexp"
-    "html"
 )
 
 func parseLinks(data []byte) []string {
 	re := regexp.MustCompile(`(http://download.in.gov.br/[^\'"]*)`)
 	preLinks := re.FindAllString(string(data), -1)
-    links := make([]string, len(preLinks))
-	for i, url := range links {
-        fmt.Printf("i = %d, url = %v\n", i, url)
-        links[i] = html.UnescapeString(url)
+	links := make([]string, len(preLinks))
+	for i, url := range preLinks {
+		links[i] = html.UnescapeString(url)
 	}
 	return links
 }
